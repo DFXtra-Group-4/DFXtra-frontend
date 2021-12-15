145f1cc87aa4e9c652fbc8ffe4877f2d381744c9
Deekshana Rangan <deekshanarangan@Deekshanas-MacBook-Pro.local>
2021-12-15 10:25:03 +0000

    talent page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import ScoreCard from './components/ScoreCard';
+import TalentCard from './components/Talent';
+
 
 function App() {
   return (
+    // <Router>
     <div className="App">
+    
+      <h1>Talent Spotlight</h1>
+      <div className='spotlightInfo'> </div><p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia libero ipsam pariatur, exercitationem dignissimos error voluptatem provident optio? Labore, libero. Mollitia quaerat rem officia, quasi accusamus tempore minus voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia libero ipsam pariatur, exercitationem dignissimos error voluptatem provident optio? Labore, libero. Mollitia quaerat rem officia, quasi accusamus tempore minus voluptatibus.</p>
+      <TalentCard />
+      <ScoreCard />
+      {/* <Route path="/" exact component={TalentCard} />
+        { <Route path="/signup" exact component={Score}/> }
+       */}
     </div>
+    // </Router>
   );
 }
 
