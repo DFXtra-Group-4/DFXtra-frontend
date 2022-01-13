8b2bad5bae0dd129b8f310a54903514bf4dda8e4
Nikesh-A97 <Nikeshasokcumar@hotmail.com>
2022-01-13 13:00:31 +0000

    withoutcors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,158 +12,173 @@ import Profile from "./components/profile";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import CompanyProfile from "./components/CompanyProfile";
 
 function App() {
-	const [profileData, setProfileData] = useState({});
-	const onRefresh = JSON.parse(localStorage.getItem("user"));
-	const [login, setLogin] = useState(onRefresh);
-	const [allProfileData, setAllProfileData] = useState([]);
-	const [allCompanyData, setAllCompanyData] = useState({});
+  const [profileData, setProfileData] = useState({});
+  const onRefresh = JSON.parse(localStorage.getItem("user"));
+  const [login, setLogin] = useState(onRefresh);
+  const [allProfileData, setAllProfileData] = useState([]);
+  const [allCompanyData, setAllCompanyData] = useState({});
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const getAllProfileData = async () => {
-		try {
-			const res = await axios.get(
-				//`${process.env.REACT_APP_URL}/trainees`
-				"https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/trainees"
-			);
-			return res.data;
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  const getAllProfileData = async () => {
+    try {
+      const res = await axios.get(
+        //`${process.env.REACT_APP_URL}/trainees`
+        "http://127.0.0.1:4000/trainees"
+      );
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	const getProfileData = async () => {
-		try {
-			const res = await axios.get(
-				`https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/trainee/${login.email}`
-			);
-			return res.data;
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  const getProfileData = async () => {
+    try {
+      const res = await axios.get(
+        `http://127.0.0.1:4000/trainee/${login.email}`
+      );
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	const getCompanyData = async () => {
-		try {
-			console.log("making GET request...");
-			const res = await axios.get(
-				`https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/vacancies`
-			);
-			console.log("company data is...", res.data);
-			return res.data;
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  const getCompanyData = async () => {
+    try {
+      console.log("making GET request...");
+      const res = await axios.get(`http://127.0.0.1:4000/vacancies`);
+      console.log("company data is...", res.data);
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	useEffect(() => {
-		const getData = async () => {
-			setAllProfileData(await getAllProfileData());
-			setProfileData(await getProfileData());
-			setAllCompanyData(await getCompanyData());
-		};
-		getData();
-		console.log("login is..", login);
-	}, [login]);
+  useEffect(() => {
+    const getData = async () => {
+      setAllProfileData(await getAllProfileData());
+      setProfileData(await getProfileData());
+      setAllCompanyData(await getCompanyData());
+    };
+    getData();
+    console.log("login is..", login);
+  }, [login]);
 
-	const updateData = async data => {
-		try {
-			await axios.put(
-				`https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/trainee/${login.email}/edit`,
-				data
-			);
-		} catch (e) {
-			console.log(e);
-		} finally {
-			setProfileData(await getProfileData());
-		}
-	};
+  const updateData = async (data) => {
+    try {
+      await axios.put(
+        `http://127.0.0.1:4000/trainee/${login.email}/edit`,
+        data
+      );
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setProfileData(await getProfileData());
+    }
+  };
 
-	const [delRequest, setDelRequest] = useState({});
-	const sendDelRequest = async id => {
-		try {
-			await axios.put(
-				`https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/trainee/${login.email}/edit/delete`,
-				id
-			);
-		} catch (e) {
-			console.log(e);
-		} finally {
-			setDelRequest({ id: id });
-			setProfileData(await getProfileData());
-		}
-	};
+  const [delRequest, setDelRequest] = useState({});
+  const sendDelRequest = async (id) => {
+    try {
+      await axios.put(
+        `http://127.0.0.1:4000/trainee/${login.email}/edit/delete`,
+        id
+      );
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setDelRequest({ id: id });
+      setProfileData(await getProfileData());
+    }
+  };
 
-	const navigateTo = string => {
-		navigate(string);
-	};
+  const navigateTo = (string) => {
+    navigate(string);
+  };
 
-	return (
-		<>
-			<Navbar data={profileData} navigateTo={navigateTo} />
+  return (
+    <>
+      <Navbar data={profileData} navigateTo={navigateTo} />
 
-			<Routes>
-				<Route
-					path="/talent"
-					exact
-					element={<TalentCard allProfileData={allProfileData} navigateTo={navigateTo} />}
-				/>
-				<Route
-					path="/score"
-					exact
-					element={<ScoreCard data={allProfileData} navigateTo={navigateTo} />}
-				/>
-				<Route
-					path="/trainee/:_id"
-					exact
-					element={<Profile data={profileData} navigateTo={navigateTo} />}
-				/>
-				<Route
-					path="/trainee/:_id/edit"
-					exact
-					element={
-						<EditProfile
-							profileData={profileData}
-							updateData={updateData}
-							navigateTo={navigateTo}
-							sendDelRequest={sendDelRequest}
-						/>
-					}
-				/>
-				<Route
-					path="/landing"
-					exact
-					element={<IndustryLanding data={allProfileData} navigateTo={navigateTo} />}
-				/>
-				<Route
-					path="/vacancies"
-					exact
-					element={<Vacancies navigateTo={navigateTo} allCompanyData={allCompanyData} />}
-				/>
-				<Route
-					path="/company/:companyName"
-					exact
-					element={<CompanyProfile allCompanyData={allCompanyData} />}
-				/>
+      <Routes>
+        <Route
+          path="/talent"
+          exact
+          element={
+            <TalentCard
+              allProfileData={allProfileData}
+              navigateTo={navigateTo}
+            />
+          }
+        />
+        <Route
+          path="/score"
+          exact
+          element={<ScoreCard data={allProfileData} navigateTo={navigateTo} />}
+        />
+        <Route
+          path="/trainee/:_id"
+          exact
+          element={<Profile data={profileData} navigateTo={navigateTo} />}
+        />
+        <Route
+          path="/trainee/:_id/edit"
+          exact
+          element={
+            <EditProfile
+              profileData={profileData}
+              updateData={updateData}
+              navigateTo={navigateTo}
+              sendDelRequest={sendDelRequest}
+            />
+          }
+        />
+        <Route
+          path="/landing"
+          exact
+          element={
+            <IndustryLanding data={allProfileData} navigateTo={navigateTo} />
+          }
+        />
+        <Route
+          path="/vacancies"
+          exact
+          element={
+            <Vacancies
+              navigateTo={navigateTo}
+              allCompanyData={allCompanyData}
+            />
+          }
+        />
+        <Route
+          path="/company/:companyName"
+          exact
+          element={<CompanyProfile allCompanyData={allCompanyData} />}
+        />
 
-				<Route
-					path="/"
-					exact
-					element={
-						<Login
-							setLogin={setLogin}
-							allProfileData={allProfileData}
-							loginState={login}
-						/>
-					}
-				/>
-			</Routes>
-		</>
-	);
+        <Route
+          path="/"
+          exact
+          element={
+            <Login
+              setLogin={setLogin}
+              allProfileData={allProfileData}
+              loginState={login}
+            />
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -8,161 +8,161 @@ import axios from "axios";
 import validateInputs from "./utils/inputValidations";
 
 const Login = ({ setLogin, allProfileData, loginState }) => {
-	const form = useRef();
-	const checkBtn = useRef();
+  const form = useRef();
+  const checkBtn = useRef();
 
-	const [loading, setLoading] = useState(false);
-	const [message, setMessage] = useState(``);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(``);
 
-	const [user, setUser] = useState({
-		email: "",
-		password: ""
-	});
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
 
-	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	const onChangeEmail = e => {
-		const newEmail = e.target.value;
-		setUser(prevstate => ({
-			...prevstate,
-			email: newEmail
-		}));
-		console.log(user);
-	};
+  const onChangeEmail = e => {
+    const newEmail = e.target.value;
+    setUser(prevstate => ({
+      ...prevstate,
+      email: newEmail
+    }));
+    console.log(user);
+  };
 
-	const onChangePassword = e => {
-		const newPassword = e.target.value;
-		setUser(prevstate => ({
-			...prevstate,
-			password: newPassword
-		}));
-		console.log(user);
-	};
+  const onChangePassword = e => {
+    const newPassword = e.target.value;
+    setUser(prevstate => ({
+      ...prevstate,
+      password: newPassword
+    }));
+    console.log(user);
+  };
 
-	const filterProfile = data => {
-		console.log("login filter data input", data.email);
-		console.log("allprofiles", allProfileData);
-		const filtered = allProfileData.filter(
-			profiles => profiles.personalDetails.contact.email.workEmail === data.email
-		);
-		console.log("fitlered is ", filtered);
-		return filtered[0]._id;
-	};
+  const filterProfile = data => {
+    console.log("login filter data input", data.email);
+    console.log("allprofiles", allProfileData);
+    const filtered = allProfileData.filter(
+      profiles => profiles.personalDetails.contact.email.workEmail === data.email
+    );
+    console.log("fitlered is ", filtered);
+    return filtered[0]._id;
+  };
 
-	const login = async user => {
-		try {
-			const res = await axios.post(
-				`https://cors-anywhere.herokuapp.com/http://ec2-54-204-116-186.compute-1.amazonaws.com:4000/login`,
-				user
-			);
-			const data = await res.data;
-			localStorage.setItem(`user`, JSON.stringify(res.data.user));
-			return data;
-		} catch (error) {
-			console.log("error is", error);
-			return { error: error.response.data.message };
-		}
-	};
+  const login = async user => {
+    try {
+      const res = await axios.post(
+        `http://127.0.0.1:4000/login`,
+        user
+      );
+      const data = await res.data;
+      localStorage.setItem(`user`, JSON.stringify(res.data.user));
+      return data;
+    } catch (error) {
+      console.log("error is", error);
+      return { error: error.response.data.message };
+    }
+  };
 
-	const handleLogin = async e => {
-		e.preventDefault();
-		setMessage("");
-		setLoading(true);
+  const handleLogin = async e => {
+    e.preventDefault();
+    setMessage("");
+    setLoading(true);
 
-		form.current.validateAll();
+    form.current.validateAll();
 
-		if (checkBtn.current.context._errors.length === 0) {
-			const loggedInUser = await login(user);
-			const loggedInUserParam = loggedInUser.user;
-			if (localStorage.getItem("user") && loggedInUser.user.roles[0] === "Graduate") {
-				setLogin(loggedInUser.user);
-				setMessage(loggedInUser.message);
-				// alert(loggedInUser.message); // Just for testing purpose
-				navigate(`/trainee/${filterProfile(loggedInUserParam)}`);
-			} else if (localStorage.getItem("user") && loggedInUser.user.roles[0] === "Industry") {
-				setLogin(loggedInUser.user);
-				setMessage(loggedInUser.message);
-				navigate(`/company/${loggedInUser.user.name}`);
-			} else {
-				setMessage(loggedInUser.error);
-				setLoading(false);
-				// alert(loggedInUser.error); // Just for testing purpose
-			}
-		} else {
-			setLoading(false);
-		}
-	};
+    if (checkBtn.current.context._errors.length === 0) {
+      const loggedInUser = await login(user);
+      const loggedInUserParam = loggedInUser.user;
+      if (localStorage.getItem("user") && loggedInUser.user.roles[0] === "Graduate") {
+        setLogin(loggedInUser.user);
+        setMessage(loggedInUser.message);
+        // alert(loggedInUser.message); // Just for testing purpose
+        navigate(`/trainee/${filterProfile(loggedInUserParam)}`);
+      } else if (localStorage.getItem("user") && loggedInUser.user.roles[0] === "Industry") {
+        setLogin(loggedInUser.user);
+        setMessage(loggedInUser.message);
+        navigate(`/company/${loggedInUser.user.name}`);
+      } else {
+        setMessage(loggedInUser.error);
+        setLoading(false);
+        // alert(loggedInUser.error); // Just for testing purpose
+      }
+    } else {
+      setLoading(false);
+    }
+  };
 
-	// useEffect(() => {
-	//   filterProfile(JSON.parse(localStorage.getItem("user")));
-	// }, [])
+  // useEffect(() => {
+  //   filterProfile(JSON.parse(localStorage.getItem("user")));
+  // }, [])
 
-	return (
-		<>
-			{/* {localStorage.getItem("user") &&
+  return (
+    <>
+      {/* {localStorage.getItem("user") &&
         navigate(`/trainee/${filterProfile(JSON.parse(localStorage.getItem("user")).email)}`)} */}
-			<div id="nav">
-				<p className="DFX" style={{ color: "#fff" }}>
-					{"DFX"}
-				</p>
-			</div>
-			<div className="col-md-12">
-				<div className="card card-container">
-					<h1>Sign in to DFX</h1>
-					<img
-						src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-						alt="profile-img"
-						className="profile-img-card"
-					/>
+      <div id="nav">
+        <p className="DFX" style={{ color: "#fff" }}>
+          {"DFX"}
+        </p>
+      </div>
+      <div className="col-md-12">
+        <div className="card card-container">
+          <h1>Sign in to DFX</h1>
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
+          />
 
-					<Form onSubmit={handleLogin} ref={form}>
-						<div className="form-group">
-							<label htmlFor="Email">Email</label>
-							<Input
-								type="text"
-								className="form-control"
-								name="email"
-								value={user.email}
-								onChange={onChangeEmail}
-								validations={[validateInputs.required, validateInputs.validEmail]}
-							/>
-						</div>
-						<div className="form-group">
-							<label htmlFor="password">Password</label>
-							<Input
-								type="password"
-								className="form-control"
-								name="password"
-								value={user.password}
-								onChange={onChangePassword}
-							/>
-						</div>
-						<div className="form-group">
-							<button
-								className="btn btn-primary btn-block"
-								disabled={loading}
-								type="submit"
-							>
-								{loading && (
-									<span className="spinner-border spinner-border-sm"></span>
-								)}
-								<span>Login</span>
-							</button>
-						</div>
+          <Form onSubmit={handleLogin} ref={form}>
+            <div className="form-group">
+              <label htmlFor="Email">Email</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="email"
+                value={user.email}
+                onChange={onChangeEmail}
+                validations={[validateInputs.required, validateInputs.validEmail]}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={user.password}
+                onChange={onChangePassword}
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="btn btn-primary btn-block"
+                disabled={loading}
+                type="submit"
+              >
+                {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Login</span>
+              </button>
+            </div>
 
-						{message && (
-							<div className="form-group fg1">
-								<div className="alert alert-danger" role="alert">
-									{message}
-								</div>
-							</div>
-						)}
-						<CheckButton style={{ display: "none" }} ref={checkBtn} />
-					</Form>
-				</div>
-			</div>
-		</>
-	);
+            {message && (
+              <div className="form-group fg1">
+                <div className="alert alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
